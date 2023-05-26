8198c29c3fdbaead78491a6b273dd246b8075791
Shuan75 <limbboy@nate.com>
2023-05-26 15:55:32 +0900

    first commit

---
+++ src/main/java/com/example/dockerprac/controller/ShopController.java
@@ -0,0 +1,20 @@
+package com.example.dockerprac.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class ShopController {
+
+    @GetMapping("/shop")
+    public ModelAndView shop() {
+        return new ModelAndView("index");
+    }
+
+
+}
